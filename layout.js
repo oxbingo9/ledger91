a4a4cef5757e3c32f4cd463ca566d24d52486673
oxbingo9 <oxbingo@naver.com>
2026-04-09 17:38:36 +0900

    Delete layout.js

--- layout.js
+++
@@ -1,4 +0,0 @@
-export const metadata = { title: "인사 조회", description: "년도별 의장 조회" };
-export default function RootLayout({ children }) {
-  return <html lang="ko"><body style={{ margin: 0 }}>{children}</body></html>;
-}
